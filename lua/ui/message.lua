8b3840edeb6ea5113ae2f98517a73f82379b9384
Shawon <mdmoinulhossainshawon@gmail.com>
2025-04-07 20:51:40 +0600

    feat(message): Added basic confirm() message support

--- lua/ui/message.lua
+++ lua/ui/message.lua
@@ -3,6 +3,7 @@
 local message = {};
 
 local log = require("ui.log");
+local spec = require("ui.spec");
 local utils = require("ui.utils");
 
 ------------------------------------------------------------------------------
@@ -15,6 +16,9 @@ message.msg_buffer, message.msg_window = nil, nil;
 
 ---@type integer, integer Buffer & window for showing stuff.
 message.show_buffer, message.show_window = nil, nil;
+
+---@type integer, integer Buffer & window for showing stuff.
+message.confirm_buffer, message.confirm_window = nil, nil;
 
 message.state = {};
 
@@ -36,12 +40,38 @@ message.visible = {};
 ------------------------------------------------------------------------------
 
 message.__prepare = function ()
+	local win_config = {
+			relative = "editor",
+
+			row = 0, col = 0,
+			width = 1, height = 1,
+
+			style = "minimal",
+			hide = true
+	};
+
 	if not message.msg_buffer or vim.api.nvim_buf_is_valid(message.msg_buffer) == false then
 		message.msg_buffer = vim.api.nvim_create_buf(false, true);
 	end
 
+	if not message.msg_window or vim.api.nvim_win_is_valid(message.msg_window) == false then
+		message.msg_window = vim.api.nvim_open_win(message.msg_buffer, false, win_config);
+	end
+
 	if not message.show_buffer or vim.api.nvim_buf_is_valid(message.show_buffer) == false then
 		message.show_buffer = vim.api.nvim_create_buf(false, true);
+	end
+
+	if not message.show_window or vim.api.nvim_win_is_valid(message.show_window) == false then
+		message.show_window = vim.api.nvim_open_win(message.show_buffer, false, win_config);
+	end
+
+	if not message.confirm_buffer or vim.api.nvim_buf_is_valid(message.confirm_buffer) == false then
+		message.confirm_buffer = vim.api.nvim_create_buf(false, true);
+	end
+
+	if not message.confirm_window or vim.api.nvim_win_is_valid(message.confirm_window) == false then
+		message.confirm_window = vim.api.nvim_open_win(message.confirm_buffer, false, win_config);
 	end
 end
 
@@ -89,6 +119,57 @@ message.__remove = function (id)
 	end
 end
 
+------------------------------------------------------------------------------
+
+message.__confirm = function (obj)
+	local lines, exts = utils.process_content(obj.content);
+
+	vim.schedule(function ()
+		message.__prepare();
+		local config = spec.get_confirm_config(obj, lines);
+
+		local window_config = {
+			relative = "editor",
+
+			row = config.row or 5,
+			col = config.col or 5,
+
+			width = config.width or 20,
+			height = config.height or 5,
+
+			-- border = "rounded",
+			style = "minimal",
+
+			hide = false
+		};
+
+		vim.api.nvim_buf_clear_namespace(message.confirm_buffer, message.namespace, 0, -1);
+		vim.api.nvim_buf_set_lines(message.confirm_buffer, 0, -1, false, lines);
+
+		for l, line in ipairs(exts) do
+			for _, ext in ipairs(line) do
+				vim.api.nvim_buf_set_extmark(message.confirm_buffer, message.namespace, l - 1, ext[1], {
+					end_col = ext[2],
+					hl_group = ext[3]
+				});
+			end
+		end
+
+		if message.confirm_window and vim.api.nvim_win_is_valid(message.confirm_window) then
+			vim.api.nvim_win_set_config(message.confirm_window, window_config);
+		else
+			message.confirm_window = vim.api.nvim_open_win(message.confirm_buffer, false, window_config);
+		end
+
+		vim.wo[message.confirm_window].wrap = true;
+		vim.wo[message.confirm_window].linebreak = true;
+
+		if config.winhl then
+			vim.wo[message.confirm_window].winhl = config.winhl;
+		end
+	end)
+end
+
 message.__hide = function ()
 	local keys = vim.tbl_keys(message.visible);
 	if #keys ~= 0 then return; end
@@ -98,13 +179,13 @@ end
 
 message.__render = function ()
 	local keys = vim.tbl_keys(message.visible);
+
 	if #keys == 0 then
 		message.__hide();
 		return;
 	end
 
 	message.__prepare();
-
 	local lines, exts = {}, {};
 
 	for _, key in ipairs(keys) do
@@ -118,13 +199,22 @@ message.__render = function ()
 	vim.api.nvim_buf_clear_namespace(message.msg_buffer, message.namespace, 0, -1);
 	vim.api.nvim_buf_set_lines(message.msg_buffer, 0, -1, false, lines);
 
+	for l, line in ipairs(exts) do
+		for _, ext in ipairs(line) do
+			vim.api.nvim_buf_set_extmark(message.msg_buffer, message.namespace, l - 1, ext[1], {
+				end_col = ext[2],
+				hl_group = ext[3]
+			});
+		end
+	end
+
 	local window_config = {
 		relative = "editor",
 
 		row = 0,
-		col = 0,
+		col = vim.o.columns,
 
-		width = 40,
+		width = 20,
 		height = 5,
 
 		border = "rounded",
@@ -138,17 +228,30 @@ message.__render = function ()
 	else
 		message.msg_window = vim.api.nvim_open_win(message.msg_buffer, false, window_config);
 	end
+
+	vim.wo[message.msg_window].wrap = true;
+	vim.wo[message.msg_window].linebreak = true;
 end
 
 ------------------------------------------------------------------------------
 
 message.msg_show = function (kind, content, replace_last)
+	table.insert(log.entries, kind)
 	if replace_last then
 	else
-		message.__append({
-			kind = kind,
-			content = content,
-		}, 5000);
+		if kind == "confirm" then
+			local _, e = pcall(message.__confirm, {
+				kind = kind,
+				content = content,
+			});
+
+			table.insert(log.entries, e)
+		else
+			message.__append({
+				kind = kind,
+				content = content,
+			}, 5000);
+		end
 	end
 end
 
@@ -161,7 +264,8 @@ message.handle = function (event, ...)
 	local _, _ = pcall(message[event], ...);
 
 	vim.api.nvim__redraw({
-		flush = true
+		flush = true,
+		win = message.msg_window
 	});
 end
 
--- lua/ui/spec.lua
+++ lua/ui/spec.lua
@@ -33,6 +33,46 @@ spec.default = {
 				end
 			}
 		}
+	},
+
+	message = {
+		confirm = {
+			default = {
+				width = function (_, lines)
+					local w;
+
+					for _, line in ipairs(lines) do
+						if w == nil or vim.fn.strdisplaywidth(line) > w then
+							w = vim.fn.strdisplaywidth(line);
+						end
+					end
+
+					return w;
+				end,
+
+				height = function (_, lines)
+					return #lines;
+				end,
+
+				row = function (_, lines)
+					return math.ceil((vim.o.lines - #lines) / 2);
+				end,
+
+				col = function (_, lines)
+					local w;
+
+					for _, line in ipairs(lines) do
+						if not w or vim.fn.strdisplaywidth(line) > w then
+							w = vim.fn.strdisplaywidth(line);
+						end
+					end
+
+					return math.ceil((vim.o.columns - w) / 2);
+				end,
+
+				winhl = "Normal:Normal"
+			},
+		}
 	}
 };
 
@@ -75,6 +115,56 @@ spec.get_cmdline_config = function (state, lines)
 
 			if can_run and val ~= nil then
 				output[k] = val;
+			else
+				output[k] = nil;
+			end
+		end
+	end
+
+	return output;
+
+	---|fE
+end
+
+spec.get_confirm_config = function (msg, lines)
+	---|fS
+
+	local styles = spec.config.message.confirm or {};
+	local _output = styles.default or {};
+
+	---@type string[]
+	local keys = vim.tbl_keys(styles);
+	table.sort(keys);
+
+	--- Iterate over keys and get the first
+	--- match.
+	for _, key in ipairs(keys) do
+		if key == "default" then goto continue; end
+		local entry = styles[key] or {};
+		local can_validate, valid = pcall(entry.condition, msg, lines);
+
+		if can_validate and valid ~= false then
+			_output = vim.tbl_extend("force", _output, entry);
+			break;
+		end
+
+	    ::continue::
+	end
+
+	local output = {};
+
+	--- Turn dynamic values into static
+	--- values
+	for k, v in pairs(_output) do
+		if type(v) ~= "function" then
+			output[k] = v;
+		elseif k ~= "condition" then
+			local can_run, val = pcall(v, msg, lines);
+
+			if can_run and val ~= nil then
+				output[k] = val;
+			else
+				output[k] = nil;
 			end
 		end
 	end
