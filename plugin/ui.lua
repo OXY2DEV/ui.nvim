ed9b4e25dd0015116e57c06d7a25f38aaf3cc84c
Shawon <mdmoinulhossainshawon@gmail.com>
2025-04-19 16:08:39 +0600

    chore: Updated log messages

--- plugin/ui.lua
+++ plugin/ui.lua
@@ -1,14 +1,25 @@
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function ()
+		local log = require("ui.log");
+		-- vim.g.__ui_dev = true;
+
 		require("ui").setup();
 		require("ui.highlight").setup();
+
+		log.print("Attached to UI");
+		log.level_inc();
 	end
 });
 
 vim.api.nvim_create_autocmd("VimLeave", {
 	callback = function ()
+		local log = require("ui.log");
+
+		log.level_dec();
+		log.print("Exporting log");
+
 		if vim.g.__ui_dev then
-			require("ui.log").export();
+			log.export();
 		end
 	end
 });
