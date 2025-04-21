20821dd6c7ca792553a84eb456d21f02ff669362
Shawon <mdmoinulhossainshawon@gmail.com>
2025-04-21 20:47:01 +0600

    fix(popupmemu): Popup menu now changes style based on mode

--- lua/ui/popup.lua
+++ lua/ui/popup.lua
@@ -168,8 +168,8 @@ popup.__info = function (item)
 	---|fE
 end
 
---- Renders popup menu.
-popup.__render = function ()
+--- Renders menu as a strip.
+popup.__strip_renderer = function ()
 	---|fS
 
 	if #popup.state.items == 0 then
@@ -281,6 +281,136 @@ popup.__render = function ()
 		flush = true,
 		win = popup.window[tab]
 	});
+
+	---|fE
+end
+
+--- Renders menu as a completion menu.
+popup.__completion_renderer = function ()
+	---|fS
+
+	if #popup.state.items == 0 then
+		return;
+	end
+
+	popup.__prepare();
+	local W = 0;
+
+	for i, item in ipairs(popup.state.items) do
+		local item_config = spec.get_item_config(item[1], item[2], item[3], item[4]) or {};
+		local text = table.concat({
+			item_config.padding_left or "",
+			item_config.icon or "",
+			item_config.text or item[1] or "",
+			item_config.padding_right or "",
+		});
+
+		W = math.max(W, vim.fn.strdisplaywidth(text));
+
+		if i == 1 then
+			vim.api.nvim_buf_set_lines(popup.buffer, 0, -1, false, { text });
+		else
+			vim.api.nvim_buf_set_lines(popup.buffer, -1, -1, false, { text });
+		end
+
+		if (i - 1) == popup.state.selected then
+			pcall(vim.api.nvim_buf_set_extmark, popup.buffer, popup.namespace, i - 1, 0, {
+				end_col = #text,
+				line_hl_group = item_config.select_hl or "CursorLine"
+			});
+
+			popup.__info(item);
+		elseif item_config.normal_hl then
+			pcall(vim.api.nvim_buf_set_extmark, popup.buffer, popup.namespace, i - 1, 0, {
+				end_col = #text,
+				line_hl_group = item_config.normal_hl
+			});
+		end
+
+		if item_config.icon_hl then
+			pcall(vim.api.nvim_buf_set_extmark, popup.buffer, popup.namespace, i - 1, #(item_config.padding_left or ""), {
+				end_col = #(item_config.padding_left or "") + #(item_config.icon or ""),
+				hl_group = item_config.icon_hl
+			});
+		end
+	end
+
+	local tooltip = utils.eval(spec.config.popupmenu.tooltip, popup.state);
+
+	local win = vim.api.nvim_get_current_win();
+	local pos = vim.api.nvim_win_get_cursor(win);
+
+	local H = #popup.state.items;
+	local screenpos = vim.fn.screenpos(win, pos[1], pos[2]);
+
+	log.print(vim.inspect(screenpos))
+
+	local tab = vim.api.nvim_get_current_tabpage();
+	local win_config = {
+		relative = "cursor",
+
+		row = 0,
+		col = 0,
+
+		width = W,
+		height = H,
+
+		footer = tooltip,
+		footer_pos = tooltip and "right" or nil,
+
+		style = "minimal",
+		hide = false,
+		focusable = false
+	};
+
+	if screenpos.row + H >= vim.o.lines then
+		win_config.row = 0;
+
+		if screenpos.curscol + W >= vim.o.columns then
+			win_config.anchor = "SE";
+			win_config.col = 2;
+		else
+			win_config.anchor = "SW";
+			win_config.col = 1;
+		end
+	else
+		win_config.row = 1;
+
+		if screenpos.curscol + W >= vim.o.columns then
+			win_config.anchor = "NE";
+			win_config.col = 2;
+		else
+			win_config.anchor = "NW";
+			win_config.col = 1;
+		end
+	end
+
+	pcall(vim.api.nvim_win_set_config, popup.window[tab], win_config);
+	pcall(vim.api.nvim_win_set_cursor, popup.window[tab], { popup.state.selected + 1, 0 });
+
+	vim.api.nvim__redraw({
+		flush = true,
+		win = popup.window[tab]
+	});
+
+	---|fE
+end
+
+--- Renders popup menu.
+popup.__render = function ()
+	---|fS
+
+	local mode = vim.api.nvim_get_mode().mode;
+
+	if mode == "c" then
+		popup.__strip_renderer();
+	else
+		popup.__completion_renderer();
+	end
+
+	if package.loaded["ui.cmdline"] then
+		pcall(package.loaded["ui.cmdline"].__render)
+	end
 
 	---|fE
 end
